b096fe40a9a241188e423c23c8f4c2d1e3b6b858
Kyle Chinick <kylechinick@icloud.com>
2022-07-13 14:35:55 -0700

    WIP add UI branching logic to add and view tasks
    Co-authored-by: Joe Roaden <josephroaden@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using ToDoList.Models;
 
-namespace ToDoList.Models
+namespace ToDoList
 {
   public class Program
   {
@@ -10,9 +10,49 @@ namespace ToDoList.Models
     {
       Console.WriteLine("Welcome to To Do List");
       Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-      string userAdd = Console.ReadLine();
-        
+      string userResponse = Console.ReadLine();
+
+      if (userResponse == "Add" || userResponse == "add")
+      {
+        Console.WriteLine("What would you like to add?");
+        string itemToAdd = Console.ReadLine();
+        Item newItem = new Item(itemToAdd);
+        List<Item> newList = new List<Item> { newItem };
+        Console.WriteLine(newItem.Description + " has been added to your ToDo list!");
+
+        Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+        userResponse = Console.ReadLine();
+          if (userResponse == "View" || userResponse == "view")
+          {
+            foreach (Item task in newList)
+          {
+            Console.WriteLine(task.Description);
+          }
+          }
+          else {
+            Console.WriteLine("Are you finished with this program (Yes/No)");
+            string finished = Console.ReadLine();
+            if (finished == "yes" || finished == "Yes")
+            {Console.WriteLine("See you later");
+            }
+            else {
+              Main();
+            }
+          }
+      }
+        else {
+          Console.WriteLine("You just started the app, you have no list, wompwomp");
+
+          // List<Item> newList = new List<Item> {};
+          // Console.WriteLine(newList[0]);
+          // foreach (Item task in newList)
+          // {
+          //   Console.WriteLine("line 27 reached");
+          //   Console.WriteLine(task.Description);
+          // }
           
+        }
+          //Main();
     }
   }
 }
