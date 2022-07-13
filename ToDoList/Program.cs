cd99cf7e700e5aab9953a429700454217af40da8
Kyle Chinick <kylechinick@icloud.com>
2022-07-13 11:53:28 -0700

    WIP add initial user interface logic
    Co-authored-by: Joe Roaden <josephroaden@gmail.com>

---
+++ ToDoList/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Models
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to To Do List");
+      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+      string userAdd = Console.ReadLine();
+        
+          
+    }
+  }
+}
